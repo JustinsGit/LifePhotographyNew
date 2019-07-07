f329a041dc031c955012717759bfab7f3fbc7c66
JustinsGit <justin.j0302@gmail.com>
2019-07-07 08:37:58 -0600

    Added logic code for job/blog apps

--- LifePhotography/settings.py
+++ LifePhotography/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,13 @@ WSGI_APPLICATION = 'LifePhotography.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "lifephotographydb",
+        'USER': "Justin",
+        'PASSWORD': "warrior0302",
+        'HOST': '',
+        'PORT': '',
+
     }
 }
 
@@ -120,3 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
